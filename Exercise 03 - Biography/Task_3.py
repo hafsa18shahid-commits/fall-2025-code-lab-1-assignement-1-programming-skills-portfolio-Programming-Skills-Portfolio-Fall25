d5578c68aea370b64b4ae2347f1093f36d403297
hafsa18shahid-commits <hafsa18shahid@gmail.com>
2025-11-24 09:58:10 +0400

    i finished it

---
+++ Exercise 03 - Biography/Task_3.py
@@ -0,0 +1,25 @@
+#Basic
+Personal_info = {
+    "name": "Hafsa Shahid",
+    "hometown": "Lahore",
+    "age": "19"
+}
+
+print(Personal_info["name"])
+print(Personal_info["hometown"])
+print(Personal_info["age"])
+#advanced version
+
+name = input("Enter your name: ")
+hometown = input("Enter your hometown: ")
+age = input("Enter your age: ")
+
+Personal_info = {
+    "name": name,
+    "hometown": hometown,
+    "age": age
+}
+
+print(Personal_info["name"])
+print(Personal_info["hometown"])
+print(Personal_info["age"])
---
+++ Exercise 08 - Simple Search/Task_8.py
@@ -0,0 +1,8 @@
+names = ["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
+search_name = "Sam"
+
+# Checking
+if search_name in names:
+    print(search_name, "was found in the list!")
+else:
+    print(search_name, "was NOT found in the list.")
